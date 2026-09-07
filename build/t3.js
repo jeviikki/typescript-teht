c44af7d54e4d47a7ffb424c3d356001dac99c503
Jeviikki <jeviik24@gmail.com>
2026-09-07 20:53:20 +0300

    exercise 3 completed

---
+++ build/t3.js
@@ -0,0 +1,18 @@
+"use strict";
+//export {}; // hack to ignore Book from task 4
+function promptForBook() {
+    const bookTitle = String(prompt("Enter the book's title"));
+    const bookAuthor = String(prompt("Enter the book's author"));
+    const bookPublicationYear = Number(prompt("Enter the book's publication year"));
+    const book = {
+        title: bookTitle,
+        author: bookAuthor,
+        publicationYear: bookPublicationYear,
+    };
+    return book;
+}
+const bookDetails = promptForBook();
+console.log("Book Details:");
+console.log(`Title: ${bookDetails.title}`);
+console.log(`Author: ${bookDetails.author}`);
+console.log(`Publication Year: ${bookDetails.publicationYear}`);
---
+++ task3/src/t3.ts
@@ -0,0 +1,27 @@
+//export {}; // hack to ignore Book from task 4
+
+type Book = {
+    title: string,
+    author: string,
+    publicationYear: number,
+}
+
+function promptForBook() {
+    const bookTitle : string = String(prompt("Enter the book's title"));
+    const bookAuthor : string = String(prompt("Enter the book's author"));
+    const bookPublicationYear : number = Number(prompt("Enter the book's publication year"));
+
+    const book: Book = {
+        title: bookTitle,
+        author: bookAuthor,
+        publicationYear: bookPublicationYear,
+    };
+    return book;
+}
+
+const bookDetails = promptForBook();
+
+console.log("Book Details:");
+console.log(`Title: ${bookDetails.title}`);
+console.log(`Author: ${bookDetails.author}`);
+console.log(`Publication Year: ${bookDetails.publicationYear}`);
