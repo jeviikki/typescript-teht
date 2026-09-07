8dcd0e11137c03df874f3bb78a9724d87e352a1c
Jeviikki <jeviik24@gmail.com>
2026-09-07 20:17:23 +0300

    completed exercise 2

---
+++ task2/src/t2.ts
@@ -0,0 +1,26 @@
+
+// parameter num should be a number or null or undefined and the function shoud return a number or a string
+function squareRoot(num: number | undefined) {
+    if (num == null || undefined) {
+        return "Input is undefined or null."
+    }
+    if (!Number(num)) {
+        return "Invalid input. Please enter a valid number."
+    }
+    if (num < 0) {
+        return "Cannot calculate square root of a negative number."
+    }
+    // Calculate the square root and return the result
+    const sqrt = Math.sqrt(num);
+    return sqrt;
+}
+
+// Prompt the user to enter a number
+const userInput = prompt("Enter a number");
+
+// Convert user input to a number or keep it undefined if empty
+const numberInput: number | undefined = userInput ? parseFloat(userInput) : undefined;
+
+// Call the squareRoot function and display the result
+const result = squareRoot(numberInput);
+console.log(result);
