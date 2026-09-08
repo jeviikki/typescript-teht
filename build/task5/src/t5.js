ab18d95d7c7e98289c82d83165697cda1bb83b6e
Jeviikki <jeviik24@gmail.com>
2026-09-08 18:01:23 +0300

    completed exercise 5

---
+++ build/task5/src/t5.js
@@ -0,0 +1,22 @@
+"use strict";
+// define the type(s) for 'value'
+function lengthOrSquare(value) {
+    let response;
+    if (typeof value == "string") {
+        // if type is string, retrurn the length of the string
+        const response = `${value.length}`;
+        return response;
+    }
+    else if (typeof value == "number") {
+        // if type is number return the square of the number
+        const response = Math.sqrt(value);
+        return response;
+    }
+}
+// Prompt the user to enter a value as either a string or a number
+const userInput = prompt("Enter either a number or a string");
+const parsedValue = Number(userInput) || userInput;
+// Call the lengthOrSquare function
+const result = lengthOrSquare(parsedValue);
+console.log(typeof result);
+console.log(result);
---
+++ task5/src/t5.ts
@@ -0,0 +1,22 @@
+// define the type(s) for 'value'
+function lengthOrSquare(value: string | number){
+    let response
+    if (typeof value == "string") {
+        // if type is string, retrurn the length of the string
+        const response: string = `${value.length}`;
+        return response
+    } else if (typeof value == "number") {
+        // if type is number return the square of the number
+        const response: number = Math.sqrt(value);
+        return response
+    }
+}
+
+// Prompt the user to enter a value as either a string or a number
+const userInput: string | number = prompt("Enter either a number or a string");
+const parsedValue = Number(userInput) || userInput;
+
+// Call the lengthOrSquare function
+const result = lengthOrSquare(parsedValue);
+console.log(typeof result);
+console.log(result);
